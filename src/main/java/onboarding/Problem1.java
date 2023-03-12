5a215e435450a6babce7d9650c57206e3e434db5
Choi-JJunho <junho5336@gmail.com>
2023-03-12 18:15:02 +0900

    feat: 미션 1 완료

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         System.out.println("엄청난 기능");
+        System.out.println("미션 완료");
         return answer;
     }
 }
