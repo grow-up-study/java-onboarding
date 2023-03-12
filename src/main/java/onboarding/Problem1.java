72d6a64012974f83665c349bb59e6236b3208a08
dradnats1012 <herjebi1012@gmail.com>
2023-03-12 18:12:29 +0900

    feat : 엄청난 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        System.out.println("엄청난 기능");
         return answer;
     }
 }
