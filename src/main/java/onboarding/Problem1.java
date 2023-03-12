8ac4c709f300b88d421b6d3d4469106f1eb6fa10
Choi-JJunho <junho5336@gmail.com>
2023-03-12 18:26:59 +0900

    refactor: bufferedWriter 방식으로 변경함

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,12 +1,17 @@
 package onboarding;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+    public static int solution(List<Integer> pobi, List<Integer> crong) throws IOException {
         int answer = Integer.MAX_VALUE;
-        System.out.println("엄청난 기능");
-        System.out.println("미션 완료");
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        bufferedWriter.write("안녕하세요");
+        bufferedWriter.flush();
+        bufferedWriter.close();
         return answer;
     }
 }
