8792793884be7416d71d6bb9ef8cc7b400553eae
dradnats1012 <herjebi1012@gmail.com>
2023-03-16 00:07:55 +0900

    Problem4 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
-
+import java.lang.StringBuilder;
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        StringBuilder encrypt = new StringBuilder(word);
+        encryption(encrypt);
+        String answer = encrypt.toString();
         return answer;
+    }
+    public static StringBuilder encryption(StringBuilder encrypt){
+
+        for (int k = 0; k < encrypt.length(); k++) {
+            if (encrypt.charAt(k) >= 65 && encrypt.charAt(k) <= 90) {
+                int x = 155 - encrypt.charAt(k);
+                encrypt.setCharAt(k, (char) x);
+            }
+            if (encrypt.charAt(k) >= 97 && encrypt.charAt(k) <= 122) {
+                int y = 219 - encrypt.charAt(k);
+                encrypt.setCharAt(k, (char) y);
+            }
+        }
+        return encrypt;
     }
 }
