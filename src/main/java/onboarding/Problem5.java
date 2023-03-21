1abef86b0b630b4f62fec498f140e9340f1defbf
dradnats1012 <herjebi1012@gmail.com>
2023-03-21 23:26:55 +0900

    Problem5 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,55 +1,49 @@
+/**
+ * 이 클래스는 현금 변환을 위한 클래스다.
+ * 금액이 큰 단위로 변환하는 기능에 대한 콘트롤은 change 메소드를 사용하여 수행한다.
+ *
+ * @version 1.00 03/21/23
+ * @author 허준기
+ */
 package onboarding;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer =change(money);
+        List<Integer> answer = change(money);
         return answer;
     }
-    public static List<Integer> change(int money){
+
+    public static List<Integer> change(int money) {
         List<Integer> devide = new ArrayList<Integer>();
-        int k = 0;
-        if (money / 10000 >= 5){
-            devide.add(money/50000);
-            money -= 50000 * (money/50000);
+        int monetaryUnit = 50000;
+        int moneytaryDigit;
+        while (monetaryUnit >= 1) {
+            moneytaryDigit = (int) (Math.log10(monetaryUnit));
+            if (money >= monetaryUnit) {
+                devide.add(money / monetaryUnit);
+                money -= monetaryUnit * (money / monetaryUnit);
+                if (monetaryUnit / (Math.pow(10, moneytaryDigit)) == 5) {
+                    monetaryUnit /= 5;
+                } else if (monetaryUnit == 10) {
+                    monetaryUnit /= 10;
+                } else {
+                    monetaryUnit /= 2;
+                }
+            } else {
+                devide.add(0);
+                moneytaryDigit = (int) (Math.log10(monetaryUnit));
+                if (monetaryUnit / (Math.pow(10, moneytaryDigit)) == 5) {   //(10 ^ (moneytaryDigit))로 자리수 구했더니 오류남..
+                    monetaryUnit /= 5;
+                } else if (monetaryUnit == 10) {
+                    monetaryUnit /= 10;
+                } else {
+                    monetaryUnit /= 2;
+                }
+            }
         }
-        else devide.add(0);
-        if (money / 10000 < 5){
-            devide.add(money/10000);
-            money -= 10000 * (money/10000);
-        }
-        else devide.add(0);
-        if (money / 1000 >= 5){
-            devide.add(money/5000);
-            money -= 5000 * (money/5000);
-        }
-        else devide.add(0);
-        if (money / 1000 < 5){
-            devide.add(money/1000);
-            money -= 1000 * (money/1000);
-        }
-        else devide.add(0);
-        if (money / 100 >= 5){
-            devide.add(money/500);
-            money -= 500 * (money/500);
-        }
-        else devide.add(0);
-        if (money / 100 < 5){
-            devide.add(money/100);
-            money -= 100 * (money/100);
-        }
-        else devide.add(0);
-        if (money / 10 >= 5){
-            devide.add(money/50);
-            money -= 50 * (money/50);
-        }
-        else devide.add(0);
-        if (money / 10 < 5){
-            devide.add(money/10);
-            money -= 10 * (money/10);
-        }
-        devide.add(money);
         return devide;
     }
 }
