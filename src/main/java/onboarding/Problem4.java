bf880305179fa365722934ebebb576b027e3b91c
dradnats1012 <herjebi1012@gmail.com>
2023-03-21 23:26:46 +0900

    Problem4 수정

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,5 +1,12 @@
+/**
+ * 이 클래스는 문장 암호화를 위한 클래스다.
+ * 암호화 기능에 대한 콘트롤은 encryption 메소드를 사용하여 수행한다.
+ *
+ * @version 1.00 03/21/23
+ * @author 허준기
+ */
 package onboarding;
-import java.lang.StringBuilder;
+
 public class Problem4 {
     public static String solution(String word) {
         StringBuilder encrypt = new StringBuilder(word);
@@ -7,7 +14,8 @@ public class Problem4 {
         String answer = encrypt.toString();
         return answer;
     }
-    public static StringBuilder encryption(StringBuilder encrypt){
+
+    public static StringBuilder encryption(StringBuilder encrypt) {
         for (int k = 0; k < encrypt.length(); k++) {
             if (encrypt.charAt(k) >= 65 && encrypt.charAt(k) <= 90) {
                 int x = 155 - encrypt.charAt(k);
