e7d019ed1772f4c6d59171c4291d9eba46954580
dradnats1012 <herjebi1012@gmail.com>
2023-03-22 11:40:46 +0900

    Problem2 인덱스 이름 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -17,14 +17,14 @@ public class Problem2 {
     }
 
     public static StringBuilder deleteDuplicate(StringBuilder crypArray, int cryptogram_length) {       // 중복되는 문자 삭제하는 클래스
-        int k = 0;                                                                  // 인덱스 0부터 시작
-        while (k < cryptogram_length - 1) {                                                     // k는 길이-2의 인덱스까지만 검사
-            if (crypArray.charAt(k) == crypArray.charAt(k + 1)) {                   // k와 그 다음 인덱스의 문자가 같다면
-                crypArray = crypArray.delete(k, k + 2);                                // k와 그 다음 인덱스 문자 삭제
-                k = -1;                                                             // k = -1로 초기화
+        int index = 0;                                                                  // 인덱스 0부터 시작
+        while (index < cryptogram_length - 1) {                                                     // k는 길이-2의 인덱스까지만 검사
+            if (crypArray.charAt(index) == crypArray.charAt(index + 1)) {                   // k와 그 다음 인덱스의 문자가 같다면
+                crypArray = crypArray.delete(index, index + 2);                                // k와 그 다음 인덱스 문자 삭제
+                index = -1;                                                             // k = -1로 초기화
                 cryptogram_length = crypArray.length();                                         // 문자열 길이 다시 측정
             }
-            k += 1;
+            index += 1;
         }                                                                           //정규표현식?? -> 문자열을 탐색하는 방법
         return crypArray;                                                           // 최종 문자열 StringBuilder로 반환
     }
